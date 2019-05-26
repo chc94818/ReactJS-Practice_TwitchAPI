57c26a5d05be416ec1f7b88ad07e2bf0df1184cd
Woof <ts00140421@gmail.com>
2019-05-26 21:07:06 +0800

    touch effect

--- src/components/Directory/LoadButton.js
+++ src/components/Directory/LoadButton.js
@@ -17,9 +17,14 @@ const ButtonContainer = styled.div`
         color : white;
         }
     }
+    @media (hover:none) {
+        background: ${props => props.touch? '#6E6E6E':'#4E4E4E'};
+        color: ${props => props.touch? 'white':'#C9C9C9'};
+    }
     
     cursor : pointer;
 `;
+
 
 const LoadArrow = styled.div`
     display: inline-block;
@@ -31,14 +36,28 @@ const LoadArrow = styled.div`
 `;
 
 class LoadButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            touch: false,
+        };
+        this.ToggleHandler = this.ToggleHandler.bind(this);
+    }
+
+    ToggleHandler() {
+        this.setState({touch: !this.state.touch});
+    }
 
     render() {
-        const{
+        const {
             handler
         } = this.props;
         return (
             <ButtonContainer
                 onClick={handler}
+                onTouchStart={this.ToggleHandler}
+                onTouchEnd={this.ToggleHandler}
+                touch={this.state.touch}
             >
                 <LoadArrow/>
             </ButtonContainer>
